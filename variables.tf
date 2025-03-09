9de2dc558b9d2395679ca381e98fcdce02c3b0e8
Srushti <srushtijiyani2013@gmail.com>
2025-03-09 19:47:32 -0400

    Added root modules variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+# File: variables.tf
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group."
+  default     = "n9400-ResourceGroup"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where the resource group will be created."
+  default     = "canadacentral"
+}
+
+variable "prefix" {
+  type    = string
+  default = "n9400"
+}
