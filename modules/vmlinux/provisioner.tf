3ed926e249a6616cc81161a4462cc341e4858cdd
Srushti <srushtijiyani2013@gmail.com>
2025-04-03 16:24:48 -0400

    Updated tasks

--- modules/vmlinux/provisioner.tf
+++ modules/vmlinux/provisioner.tf
@@ -4,6 +4,7 @@
 
 resource "null_resource" "ansible_provision" {
   for_each = toset([for i in range(var.vm_linux_count) : tostring(i)])
+
   depends_on = [azurerm_linux_virtual_machine.linux-vm]
 
   connection {
@@ -14,6 +15,6 @@ resource "null_resource" "ansible_provision" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${azurerm_public_ip.linux-pip[each.key].fqdn}, ./ansible/n01669400-playbook.yml"
+    command = "ansible-playbook -i ./ansible/inventory.ini ./ansible/n01669400-playbook.yml"
   }
 }
